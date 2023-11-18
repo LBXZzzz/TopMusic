ade7b41065a0046d70678800ab13a5068baca512
蜡笔小朱搞安卓 <1695677658@qq.com>
2023-11-19 00:20:06 +0800

    feat:主界面添加了导航栏

--- app/src/main/java/com/example/topmusic/MainActivity.kt
+++ app/src/main/java/com/example/topmusic/MainActivity.kt
@@ -2,16 +2,32 @@ package com.example.topmusic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.common.base.BaseActivity
 import com.example.topmusic.databinding.ActivityMainBinding
 
 
 class MainActivity : com.example.common.base.BaseActivity<ActivityMainBinding>() {
+
+    private lateinit var navController: NavController
     override val layout: Int
         get() = R.layout.activity_main
 
     override fun onActivityCreate(savedInstanceState: Bundle?) {
-        binding.ppp.text = "你好"
+        initNav()
     }
+
+    private fun initNav() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_top_music_host) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.navBt, navController)
+    }
+
 
 }
